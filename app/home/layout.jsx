9a6d6703290ce4ac567dfdb88e8f71e0ed7643ff
unknown <crayonno.o@gmail.com>
2023-04-17 08:54:43 +0300

    prog

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,4 +1,4 @@
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/footer/Footer";
 import styles from "./layout.module.css";
 
 function RootLayout({ children }) {
